ba7af7a057dfd473e0ffc6b7a26f399ad2e77f69
Kaio Silveira <silveira.kaio@icloud.com>
2024-11-23 12:32:34 +0000

    add a protection against undefined `monthlyCost`s at `Party`

--- src/party/index.js
+++ src/party/index.js
@@ -1,5 +1,16 @@
+export class SubclassResponsibilityError extends Error {
+  constructor() {
+    super('This should be implemented by a subclass');
+    this.name = 'SubclassResponsibilityError';
+  }
+}
+
 export class Party {
   get annualCost() {
     return this.monthlyCost * 12;
+  }
+
+  get monthlyCost() {
+    throw new SubclassResponsibilityError();
   }
 }
--- src/party/index.test.js
+++ src/party/index.test.js
@@ -7,9 +7,22 @@ describe('Party', () => {
         super();
         this.monthlyCost = monthlyCost;
       }
+
+      set monthlyCost(value) {
+        this._monthlyCost = value;
+      }
+
+      get monthlyCost() {
+        return this._monthlyCost;
+      }
     }
 
     const derivedParty = new DerivedParty(10);
     expect(derivedParty.annualCost).toEqual(120);
+  });
+
+  it('should throw an error when trying to access monthlyCost', () => {
+    const party = new Party();
+    expect(() => party.monthlyCost).toThrowError('This should be implemented by a subclass');
   });
 });
